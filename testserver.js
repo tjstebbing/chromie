a9272794b7ff1c5ecccc2ab6dc97fcebd7571b73
Pomke Nohkan <pomke@pomke.com>
2012-04-29 14:36:33 +1000

    trying to get socket.io to serve

--- testserver.js
+++ testserver.js
@@ -1,8 +1,10 @@
+#!/usr/bin/node
 connect = require("connect");
 chromie = require("./lib");
 test_server = require("./test_server");
 
 
 app = connect();
+app.use(connect.errorHandler());
 portal = chromie.Portal(app, test_server.settings);
 app.listen(8000);
